9fa5fdba6d13229e856168f425d2aad23cc2fe15
Damian Patel <damian.patel2001@gmail.com>
2022-09-11 18:10:03 -0400

    DO NOT MERGE: api

--- app/src/main/java/com/example/pa1/controller/WeatherApi.kt
+++ app/src/main/java/com/example/pa1/controller/WeatherApi.kt
@@ -3,6 +3,7 @@ package com.example.pa1.controller
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import okhttp3.*
+import java.io.IOException
 
 class WeatherApi {
     private final val apiURL: String = "https://i820e967xc.execute-api.us-east-1.amazonaws.com/test/helloworld?";
@@ -12,18 +13,21 @@ class WeatherApi {
         println(latitude)
         println(longitude)
         val request = Request.Builder().url(apiURL+"latitude=${latitude}&longitude=${longitude}").build()
-        println("After request")
         var weatherData = ""
-        val response = client.newCall(request).execute()
-        println("After response")
-        val g = Gson()
-        val o = g.fromJson(response.body()?.string(), Parser::class.java)
-        println("After creating gson parser")
-        response.body()?.close()
-        println("After closing body")
-        weatherData = o.message.periods[0].detailedForecast
-        println("After getting detailed forecast")
-        println(weatherData)
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+                println("After request")
+                val g = Gson()
+                val o = g.fromJson(response.body()?.string(), Parser::class.java)
+                println("After creating gson parser")
+                response.body()?.close()
+                println("After closing body")
+                weatherData = o.message.periods[0].detailedForecast
+                println("After getting detailed forecast")
+                println(weatherData)
+            }
+        })
         return weatherData
     }
 }
